2002a4b21feb36f1e0dfbf046c73d3942131fee1
Odokura <130954302+odokura@users.noreply.github.com>
2026-06-21 16:49:34 +0900

    Remove homepage navbar and footer at layout level

---
+++ src/theme/Layout/index.tsx
@@ -0,0 +1,57 @@
+import React, {type ReactNode} from 'react';
+import clsx from 'clsx';
+import ErrorBoundary from '@docusaurus/ErrorBoundary';
+import {
+  PageMetadata,
+  SkipToContentFallbackId,
+  ThemeClassNames,
+} from '@docusaurus/theme-common';
+import SkipToContent from '@theme/SkipToContent';
+import AnnouncementBar from '@theme/AnnouncementBar';
+import Navbar from '@theme/Navbar';
+import Footer from '@theme/Footer';
+import LayoutProvider from '@theme/Layout/Provider';
+import ErrorPageContent from '@theme/ErrorPageContent';
+import type {Props as BaseProps} from '@theme/Layout';
+import styles from '@theme-original/Layout/styles.module.css';
+
+type Props = BaseProps & {
+  noNavbar?: boolean;
+};
+
+export default function Layout(props: Props): ReactNode {
+  const {
+    children,
+    noFooter,
+    noNavbar,
+    wrapperClassName,
+    title,
+    description,
+  } = props;
+
+  return (
+    <LayoutProvider>
+      <PageMetadata title={title} description={description} />
+
+      <SkipToContent />
+
+      <AnnouncementBar />
+
+      {!noNavbar && <Navbar />}
+      <div
+        id={SkipToContentFallbackId}
+        className={clsx(
+          ThemeClassNames.layout.main.container,
+          ThemeClassNames.wrapper.main,
+          styles.mainWrapper,
+          wrapperClassName,
+        )}>
+        <ErrorBoundary fallback={(params) => <ErrorPageContent {...params} />}>
+          {children}
+        </ErrorBoundary>
+      </div>
+
+      {!noFooter && <Footer />}
+    </LayoutProvider>
+  );
+}
